8d21d4de08bd26532824122002a3b3511c8fe2a2
hariswaroop <hariswaroop.gangwar@gmail.com>
2012-07-14 23:16:47 +0530

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsampleapp::Application.config.secret_token = 'c465c6bfe4e46a35d091c83a6e88e18a52ad23addbda82e1e073bca9c759fcd8b79961243b1039a7949f917c444dd24704cb3fbab2d8f430377f7c8e5b19c3c9'
